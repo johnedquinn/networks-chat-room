859e5ad70c3dfc6908e8cb1cba9bc9a7218669f1
Patrick Bald <pbald@nd.edu>
2020-10-06 22:19:10 -0400

    initial setup for lib/ client/ and server/

---
+++ client/client.c
@@ -0,0 +1,38 @@
+/*
+Patrick Bald, John Quinn, Rob Reutiman
+pbald, jquin13, rreutima
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "lib/pg3lib.h"
+
+/*
+* @func   handle_messages
+* @desc   thread function for handling user messages
+*/
+void* handle_messages(void* arg){
+
+
+    return NULL;
+}
+
+/*
+* @func   main
+* @desc   main driver, 
+*         main thread to handle user input, server interaction
+*/
+int main(int argc, char* argv[]){
+
+
+
+
+  return 0;
+}
---
+++ server/sever.c
@@ -0,0 +1,39 @@
+/*
+Patrick Bald, John Quinn, Rob Reutiman
+pbald, jquin13, rreutima
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "lib/pg3lib.h"
+
+/*
+* @func   client_interaction
+* @desc   thread function for handling user messages
+*/
+void* client_interaction(void* arg){
+
+
+
+  return NULL;
+}
+
+/*
+* @func   main
+* @desc   main driver, 
+*         main thread for socket listener for incoming connections
+*/
+int main(int argc, char* argv[]){
+
+
+
+
+  return 0;
+}
