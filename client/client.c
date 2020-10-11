fd51ee85074877c78b28cd3aafd9e049f7c90ce8
Patrick Bald <pbald@nd.edu>
2020-10-11 13:57:00 -0400

    client compiling minus todos

--- client/client.c
+++ client/client.c
@@ -11,8 +11,11 @@ pbald, jquin13, rreutima
 #include <strings.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 
-#include "lib/pg3lib.h"
+#include "../lib/pg3lib.h"
+
+#define MAX_LINE 4096
 
 /* GLOBALS */
 int EXIT = 0;
@@ -46,14 +49,49 @@ void login(char* username){
 int main(int argc, char* argv[]){
 
   // Get server name, port, uesrname
-  char* host; int port; char* username;
+  char* host; int SERVER_PORT; char* username;
   if(argc == 4){
     host = argv[1];
-    port = atoi(argv[2]);
+	 SERVER_PORT = atoi(argv[2]);
     username = argv[3];
   } else {
     fprintf(stderr, "Usage: ./myserver [hostname] [port] [username]\n");
     exit(1);
+  }
+
+  /* Variables */
+  struct hostent *hp;
+  struct sockaddr_in sin;
+  char buf[MAX_LINE];
+  int s;
+
+  /* Translate host name into peer's IP address */
+  hp = gethostbyname(host);
+  if(!hp) {
+    fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+    exit(1);
+  }
+
+  /* Build address data structure */
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+  sin.sin_port = htons(SERVER_PORT);
+
+  /* Create Socket */
+  if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket"); 
+    exit(1);
+  }
+
+  /* Connect to server */
+  printf("Connecting to %s on port %d\n", host, SERVER_PORT);
+
+  if(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    perror("simplex-talk: connect");
+    close(s); 
+    exit(1);
+
   }
 
   while(!EXIT){
@@ -78,7 +116,7 @@ int main(int argc, char* argv[]){
       } else if (!strncmp(option, "b", 1)){
         broadcast(); // @todo write this func
       } else {
-        fprintf(stdout, "Invalid input %s\n", options);
+        fprintf(stdout, "Invalid input %s\n", option);
       }
 
     }
